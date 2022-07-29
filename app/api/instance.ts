d3757eb49c41608c4736c99ff0ac3a4d62ee9b5b
Mark <mcherepivskiy04@gmail.com>
2022-07-29 12:15:10 +0600

    fixes

--- app/api/instance.ts
+++ app/api/instance.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	baseURL:
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:3000/api'
+			: 'https://google-green.vercel.app/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
